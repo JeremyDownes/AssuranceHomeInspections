5891984c36a18161346a1dfaba9afd6eda36acca
jeremydownes <jeremydownes@hotmail.com>
2018-02-03 02:18:23 -0500

    adds responsive menu

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 	$('html').css({fontSize: fontSize})
 	$('header').css({fontSize: fontSize*3.5})
 	$('header span').css({fontSize: fontSize*2})
+
+	$(window).scroll( function () {
+		if ($('nav').position().top > window.innerHeight*0.9) {
+			$('ul li button:focus+ul, ul li button:hover+ul').css({position: 'absolute',	top: '4vh'})
+		} else {
+			$('li button:focus+ul, ul li button:hover+ul').css({position: 'absolute',	top: '-8vh'})
+		}
+	})
 })
